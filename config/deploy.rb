bde975febde41dafbee22517aa4431d8e79bb986
Andy Wang <andywang@Andys-iMac.local>
2015-02-10 15:15:39 +1030

    update

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :scm, :git
 set :repo_url, 'git@github.com:climber2002/deploy_sample.git'
 
 # setup rbenv.
-set :rbenv_type, :system
+set :rbenv_type, :user
 set :rbenv_ruby, '2.1.4'
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
